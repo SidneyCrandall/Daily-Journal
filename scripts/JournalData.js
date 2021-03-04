305cf3f32ca81f9a7e3346ae020a2c2c61f2ac05
Sidney Crandall <sdncrandall5@gmail.com>
2021-03-03 23:22:09 -0600

    fixed a spelling errors

--- scripts/JournalData.js
+++ scripts/JournalData.js
@@ -1,32 +1,31 @@
-// This is the original data.
 const journal = [
   {
      id: 1,
      date: "07/24/2025",
-     concept: "HTML & CSS",
+     Topic: "HTML & CSS",
      entry: "We talked about HTML components and how to make grid layouts with Flexbox in CSS.",
      mood: "Ok"
   },
   {
       id: 2, 
-      date: "07/28/2025",
-      concept: "Github",
+      date: "02/18/2021",
+      Topic: "GITHUB",
       entry: "We learned about working through a Github workflow.",
       mood: "Stressed"
   },
   {
       id: 3, 
       date: "02/14/2021",
-      concept: "Snow Day",
+      Topic: "SNOW DAY",
       entry: "Happy Birthday to me, we have a Snow Day.",
       mood: "Happy"
   },
   {
       id: 4,
-      date: "03/02/2021",
-      concept: "Automated World",
-      entry: "Second group project. It was a fun experience.",
-      mood: "Sick"
+      date: "03/01/2021",
+      Topic: "AUTOMATING THE WORLD",
+      entry: "Second group project.. I hope this isn't too terrifying.",
+      mood: "Anxious"
   }
 ];
 
--- scripts/JournalEntry.js
+++ scripts/JournalEntry.js
@@ -1,15 +1,12 @@
-/*
- *  Purpose: To render a single journal entry as an
- *           HTML representation of the data
- */
 export const JournalEntryComponent = (entry) => {
-    return (`
+    return (
+		`
         <section id="entry--${entry.id}" class="journalEntry">
-		<h3 class="Entry Title">${entry.concept}</h3>
-			<p class="entry-details">Date: ${entry.date}</p>
-			<p class="entry-details">concept: ${entry.concept}</p>
-			<p class="entry-details">Entry: ${entry.entry}</p>
-			<p class="entry-details">Mood ${entry.mood}</p>
+		<h3 class="journal-Title">${entry.Topic}</h3>
+			<p class="entry-items">Date: ${entry.date}</p>
+			<p class="entry-items">Entry: ${entry.entry}</p>
+			<p class="entry-items">Mood: ${entry.mood}</p>
 	    </article>
-    `)
+    `
+	)
 }
